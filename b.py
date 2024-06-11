e8ae26f241e172ae9be75fb1ab61fc1c17d08150
marwaai <Marwa.mahmoud.abdallah@gmail.com>
2024-06-11 20:13:43 +0300

    Update b.py

--- b.py
+++ b.py
@@ -30,8 +30,7 @@ db = FAISS.from_documents(texts, embeddings)
 # Initialize LLM and QA models
 llm = CTransformers(
     model="TheBloke/Llama-2-7B-Chat-GGML",
-    model_file='llama-2-7b-chat.ggmlv3.q2_K.bin',
-    callbacks=[StreamingStdOutCallbackHandler()], stream=True,
+    model_file='llama-2-7b-chat.ggmlv3.q2_K.bin', stream=True,
     prompt="You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
 )
 
